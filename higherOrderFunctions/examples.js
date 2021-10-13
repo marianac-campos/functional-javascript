b1a819ed1af258dd4ab9695ac505aabafe50e8c4
Mariana Campos <CIANDT\marianacampos@lnb024803cps.ciandt.global>
2021-10-13 12:31:36 -0300

    13.10.2021 | Estudando  'Array Iteration'

--- higherOrderFunctions/examples.js
+++ higherOrderFunctions/examples.js
@@ -1,45 +1,106 @@
-/* 
- ---  F I L T E R  ---
+/* --- F O R E A C H  ---
 
-function isEven(x){
-  return x % 2 === 0;
-};
+> method calls a function (a callback function) once for each array element
+
+const numbers = [45, 4, 9, 16, 25];
+let txt = "";
+numbers.forEach(myFunction);
+
+function myFunction(value, index, array) {
+  txt += value + "<br>";
+}
+
+> the value 'index' and 'array' can be hidden in function
+
+  function myFunction(value) {
+    txt += value + "<br>";
+  }
+
+=== syntax: array.forEach(myFunction); ===
+
+*/
+
+/* ---  M A P  --- 
+
+> method creates a new array by performing a function on each array element
+
+const numbers1 = [45, 4, 9, 16, 25];
+const numbers2 = numbers1.map(myFunction);
+
+function myFunction(value, index, array) {
+  return value * 2;
+}
+
+console.log(numbers2);
+
+> the value 'index' and 'array' can be hidden in function
+
+  function myFunction(value) {
+    return value * 2;
+  }
+
+=== syntax: array.map(myFunction); ===
+*/
+
+/* ---  F I L T E R  ---
+
+> method creates a new array with array elements that passes a test 
 
 const numbers = [12, 324, 76, 93, 5, 45, 4234];
 const evenNumbers = numbers.filter(isEven);
 
-console.log(evenNumbers);
-*/
-
-/*
- ---  L A M B D A  ---
-
-const isEven = (x) => x % 2 === 0;
-
-const numbers = [12,324,213,4,2,3,45,4234];
-const evenNumbers = numbers.filter(isEven);
+function isEven(value, index, array){
+  return x % 2 === 0;
+}
 
 console.log(evenNumbers);
+
+> 'index' and 'array' parameters can be omitted in function
+
+  function isEven(value){
+    return x % 2 === 0;
+  }
+
+=== syntax: array.filter(myFunction); ===
 */
 
-/*
- ---  M A P  ---
+/* ---  R E D U C E  ---
 
-const numbers = [1, 3, 4, 5, 6, 12, 20, 34];
-const squares = numbers.map((x) => Math.pow(x, 2));
+> method runs a function on each array element to produce (reduce it to) a single value
+> method works from left-to-right in the array
+> if you want to start from right to left use 'reduceRight()'
 
-console.log(squares);
-*/
+const numbers = [45, 4, 9, 16, 25];
+let sum = numbers.reduce(myFunction);
 
-/*
- ---  R E D U C E  ---
+function myFunction(total, value, index, array) {
+  return total + value;
+}
 
-const numbers = [2, 3, 4, 5];
-const product = numbers.reduce((acc, x) => acc * x, 1);
+console.log(sum); //return 99
 
-console.log(product);
+> the 'reduce()' method can accept an initial value: 
 
-================================
+  const numbers = [45, 4, 9, 16, 25];
+  let sum = numbers.reduceRight(myFunction, 100);
+
+  function myFunction(total, value) {
+    return total + value;
+  }
+
+  console.log(sum); //return 199
+
+> 'index' and 'array' parameters can be omitted in function
+
+  function myFunction(total, value) {
+    return total + value;
+  }
+
+=== syntax: array.reduce(myFunction, total); ===
+
+> 'total' is optional. Here would be the initial value.
+
+===========  another examples  ============
 
 > filtering only even numbers
 
@@ -72,4 +133,51 @@ function map(arr, callback){
 
 const squares = map([1, 2, 3, 4, 5], (x) => Math.pow(x, 2));
 console.log(squares);
+*/
+
+/* ---  E V E R Y  ---
+
+> method check if all array values pass a test
+
+const numbers = [45, 4, 9, 16, 25];
+let allOver18 = numbers.every(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
+
+console.log(allOver18); //return false
+
+> the value 'index' and 'array' can be hidden in function
+
+  function myFunction(value) {
+    return value > 18;
+  }
+
+=== syntax: array.every(myFunction); ===
+*/
+
+/* --- I N D E X O F  --- 
+
+> method searches an array for an element value and returns its position
+
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+let position = fruits.indexOf("Apple") + 1;
+
+console.log(position);
+
+=== syntax: array.indexOf(item, start); ===
+
+> 'item' is required. The item to search for.
+> 'start' is optional. Where to start the search.
+*/
+
+/* ---  L A M B D A  ---
+
+const isEven = (x) => x % 2 === 0;
+
+const numbers = [12,324,213,4,2,3,45,4234];
+const evenNumbers = numbers.filter(isEven);
+
+console.log(evenNumbers);
 */

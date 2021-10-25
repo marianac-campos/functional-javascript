1b7e42902968cbcbfe1f72d42c6c8d4758abac8e
Mariana Campos <marianacristinadecampos@gmail.com>
2021-10-25 09:33:49 -0300

    aulaI - tipos primitivos

---
+++ formacao-alura/fundamentos-js-basics/type-boolean.js
@@ -0,0 +1,13 @@
+// === B O O L E A N === //
+
+const primeiroNumero = "5";
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+// console.log(primeiroNumero === segundoNumero); // false
+// console.log(primeiroNumero == segundoNumero); // true
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
---
+++ formacao-alura/fundamentos-js-basics/type-number.js
@@ -0,0 +1,25 @@
+// ==== N U M B E R ==== //
+
+const meuNumero = 3;
+
+const primeiroNumero = 89;
+const segundoNumero = 26;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(`RESULTADO: ${operacaoMatematica}`);
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(`RESULTADO: ${novaOperacao}`);
+
+// NaN -> Not A Number
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero);
